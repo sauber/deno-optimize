0792d3a41c44c69e988cae026865d30735a02383
Søren Døssing <netcom@sauber.net>
2025-05-21 12:34:33 +0200

    Documention of exported class

--- src/minimize.ts
+++ src/minimize.ts
@@ -1,5 +1,6 @@
 import { Optimizer } from "./optimizer.ts";
 
+/** Using Adam optimizer find combination of parameter values for lowest output from agent */
 export class Minimize extends Optimizer {
   /** Direction for learning (-1 for minimize) */
   protected direction: number = -1;
